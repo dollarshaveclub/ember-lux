5b9d0ddde02ab960754d31fadda0ab9d0bdae681
Brian Gonzalez <me@briangonzalez.org>
2017-08-23 11:20:07 -0700

    eslint-config-dollarshaveclub

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
-'use strict';
+'use strict'
 
-module.exports = function(environment) {
+module.exports = function env (environment) {
   let ENV = {
     modulePrefix: 'dummy',
     environment,
@@ -14,15 +14,15 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
-  };
+    },
+  }
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
@@ -34,18 +34,18 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.locationType = 'none';
+    ENV.locationType = 'none'
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = '#ember-testing'
   }
 
   if (environment === 'production') {
 
   }
 
-  return ENV;
-};
+  return ENV
+}
